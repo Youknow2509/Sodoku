76f73819eaa77049e0c991a142ec7373fdbffd08
V <lytranvinh.work@gmail.com>
2024-03-23 00:51:13 +0700

    [Upgrade]: Them file test level

--- src/Test/TestLevel.java
+++ src/Test/TestLevel.java
@@ -1,2 +1,11 @@
-package src.Test;public class TestLevel {
+package src.Test;
+
+import src.Controller.LevelController;
+
+public class TestLevel {
+    public static void main(String[] args) {
+        LevelController levelController = new LevelController("admin");
+        levelController.Application();
+        return;
+    }
 }
