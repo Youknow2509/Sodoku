4ceef702f1f30a54e1a493bda76e1449540d2aa8
V <lytranvinh.work@gmail.com>
2024-03-23 14:09:15 +0700

    [ADD]: Add file view GameController

--- src/View/MainGame.java
+++ src/View/MainGame.java
@@ -16,6 +16,18 @@ public class MainGame { // TODO vẫn thiếu hiển thị cuả loop Applicatio
         this.game = game;
         showGame = new ShowGame(game);
     }
+    // Name file save
+    public void nameFileSave() {
+        System.out.print("Tên file bạn muốn lưu: ");
+    }
+    // Hien thi menu
+    public void showMenu() {
+        System.out.println("1. Save Game");
+        System.out.println("2. Continue Game");
+        System.out.println("3. Back To Home");
+        System.out.println("4. Exit");
+        System.out.print("Choose: ");
+    }
     // Hien thi listnodegame, o trong, so luot sai
     public void showGame() {
         showGame.show();
@@ -33,8 +45,16 @@ public class MainGame { // TODO vẫn thiếu hiển thị cuả loop Applicatio
         System.out.print("Nhap gia tri: ");
     }
     // Thong bao nhap sai gia tri
-    public void inputError() {
+    public void inputValueError() {
         System.out.println("Nhap sai gia tri!");
+    }
+    // Thong bao nhap sai vi tri
+    public void inputPositionError() {
+        System.out.println("Hay chon dung vi tri trong");
+    }
+    // Thong bao nhap sai dinh dang so
+    public void inputNumberFormatError() {
+        System.out.println("Vui long nhap so nguyen.");
     }
     // Thong bao game win
     public void gameWin() {
@@ -43,5 +63,9 @@ public class MainGame { // TODO vẫn thiếu hiển thị cuả loop Applicatio
     // Thong bao game over
     public void gameOver() {
         System.out.println("Game Over!");
+    }
+    // Goodbye user
+    public void goodbye(String userName) {
+        System.out.println("Goodbye " + userName + "!");
     }
 }
