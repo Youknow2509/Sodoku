e5cad8d061ee1c7cdbd825788d0b24b749a14b51
V <lytranvinh.work@gmail.com>
2024-03-23 14:12:24 +0700

    [Upgrade]: main run  debug GameController

--- src/Test/TestGameController.java
+++ src/Test/TestGameController.java
@@ -1,14 +1,18 @@
 package src.Test;
 
 import src.Controller.GameController;
+import src.Utils.Config;
 import src.Utils.HandleData;
 
 public class TestGameController {
     public static void main(String[] args) {
         //String path = "src/Data/UserTemp/v/9x9.txt";
-        String path = "src/Data/UserTemp/v/test.txt";
-        GameController gameController = new GameController(HandleData.readDataFromFile(path), "V");
+        String path = "src/Data/User/admin/testStageGame.txt";
+        GameController gameController = new GameController(HandleData.readDataFromFile(path), "admin");
         gameController.Application();
+        if (Config.getScannerInput() != null) {
+            Config.closeScannerInput();
+        }
         return;
     }
 }
