a5ab7eed768779688069ac927d023d36098cc1d6
V <lytranvinh.work@gmail.com>
2024-03-23 14:33:47 +0700

    [Upgrade]: debug add method and view to File view

--- src/Controller/LevelController.java
+++ src/Controller/LevelController.java
@@ -2,20 +2,20 @@ package src.Controller;
 
 import src.Model.Game;
 import src.Utils.HandleData;
-
 import java.io.File;
 import java.util.InputMismatchException;
 import java.util.Random;
-import java.util.Scanner;
-
-public class LevelController { // TODO chuyển hiển thị sang View
+import src.Utils.Config;
+import src.View.Level;
+public class LevelController { 
     // Variables
     private final String path = "src/Data/Game/";
-    private Scanner scanner = null;
-    //
-    private int typeGame;
-    private int level;
+    private final Level levelView = new Level();
+    // Variables input  
+    private String typeGame;
+    private String level;
     private String user;
+    private int tG, l;
     // Constructor
     public LevelController() {
         super();
@@ -24,25 +24,21 @@ public class LevelController { // TODO chuyển hiển thị sang View
         super();
         this.user = user;
     }
-
     // Applicaton
     public void Application() {
-        scanner = new Scanner(System.in);
-        System.out.println("Vui lòng chọn loại game: 9x9, 16x16.");
-        System.out.println("1. 9x9");
-        System.out.println("2. 16x16");
-        System.out.print("Chọn: ");
+        levelView.menuApplication();
+        typeGame = (Config.getScannerInput()).nextLine();
         try {
-            typeGame = scanner.nextInt();
-            if (typeGame != 1 && typeGame != 2) {
-                System.out.println("Vui lòng chọn 1 hoặc 2.");
+            tG = Integer.parseInt(typeGame);
+            if (tG != 1 && tG != 2) {
+                   levelView.inputMenuApplicationError();
                 Application();
             }
-        } catch (InputMismatchException e) {
-            System.out.println("Vui lòng nhập số nguyên.");
+        } catch (NumberFormatException e) {
+            levelView.inputNumberFormatError();
             Application();
         }
-        if (typeGame == 1) {
+        if (tG == 1) {
             handleTypeGame9x9();
         } else {
             handleTypeGame16x16();
@@ -50,24 +46,21 @@ public class LevelController { // TODO chuyển hiển thị sang View
     }
     // Handle typeGame 9x9
     private void handleTypeGame9x9() {
-        System.out.println("Vui lòng chọn mức độ:");
-        System.out.println("1. Dễ");
-        System.out.println("2. Trung bình");
-        System.out.println("3. Khó");
-        System.out.print("Chọn: ");
+        levelView.chooseTypeGame();
+        level = (Config.getScannerInput()).nextLine();
         try {
-            level = scanner.nextInt();
-            if (level != 1 && level != 2 && level != 3) {
-                System.out.println("Vui lòng chọn 1, 2 hoặc 3.");
+            l = Integer.parseInt(level);
+            if (l != 1 && l != 2 && l != 3) {
+                levelView.inputTypeGameError();
                 handleTypeGame9x9();
             }
-        } catch (InputMismatchException e) {
-            System.out.println("Vui lòng nhập số nguyên.");
+        } catch (NumberFormatException e) {
+            levelView.inputNumberFormatError();
             handleTypeGame9x9();
         }
-        if (level == 1) {
+        if (l == 1) {
             LevelToGame(path + "9x9/Ez/");
-        } else if (level == 2) {
+        } else if (l == 2) {
             LevelToGame(path + "9x9/Medial");
         } else {
             LevelToGame(path + "9x9/Hard");
@@ -75,24 +68,21 @@ public class LevelController { // TODO chuyển hiển thị sang View
     }
     // Handle typeGame 16x16
     private void handleTypeGame16x16() {
-        System.out.println("Vui lòng chọn mức độ:");
-        System.out.println("1. Dễ");
-        System.out.println("2. Trung bình");
-        System.out.println("3. Khó");
-        System.out.print("Chọn: ");
+        levelView.chooseTypeGame();
+        level = (Config.getScannerInput()).nextLine();
         try {
-            level = scanner.nextInt();
-            if (level != 1 && level != 2 && level != 3) {
-                System.out.println("Vui lòng chọn 1, 2 hoặc 3.");
+            l = Integer.parseInt(level);
+            if (l != 1 && l != 2 && l != 3) {
+                levelView.inputTypeGameError();
                 handleTypeGame9x9();
             }
-        } catch (InputMismatchException e) {
-            System.out.println("Vui lòng nhập số nguyên.");
+        } catch (NumberFormatException e) {
+            levelView.inputNumberFormatError();
             handleTypeGame9x9();
         }
-        if (level == 1) {
+        if (l == 1) {
             LevelToGame(path + "16x16/Ez/");
-        } else if (level == 2) {
+        } else if (l == 2) {
             LevelToGame(path + "16x16/Medial");
         } else {
             LevelToGame(path + "16x16/Hard");
@@ -120,19 +110,47 @@ public class LevelController { // TODO chuyển hiển thị sang View
         return path;
     }
 
-    public int getTypeGame() {
+    public Level getLevelView() {
+        return levelView;
+    }
+
+    public String getTypeGame() {
         return typeGame;
     }
 
-    public void setTypeGame(int typeGame) {
+    public void setTypeGame(String typeGame) {
         this.typeGame = typeGame;
     }
 
-    public int getLevel() {
+    public String getLevel() {
         return level;
     }
 
-    public void setLevel(int level) {
+    public void setLevel(String level) {
         this.level = level;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public int gettG() {
+        return tG;
+    }
+
+    public void settG(int tG) {
+        this.tG = tG;
+    }
+
+    public int getL() {
+        return l;
+    }
+
+    public void setL(int l) {
+        this.l = l;
     }
 }
