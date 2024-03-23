ce33d8e55187a6521fde8b1f0ff7aac5e732c480
V <lytranvinh.work@gmail.com>
2024-03-23 14:11:27 +0700

    [ADD]: Add file config - Xử dụng input dữ liêụ ở tất cả file (xử dụng create singleton) tránh lỗi đóng mở và đồng bộ

--- src/Utils/Config.java
+++ src/Utils/Config.java
@@ -1,2 +1,20 @@
-package src.Utils;public class Config {
+package src.Utils;
+
+import java.util.Scanner;
+
+public class Config {
+    // Variables
+    private static Scanner scannerInput = null;
+
+    public static Scanner getScannerInput() {
+        if (scannerInput == null) {
+            scannerInput = new Scanner(System.in);
+        }
+        return scannerInput;
+    }
+    public static void closeScannerInput() {
+        if (scannerInput != null) {
+            scannerInput.close();
+        }
+    }
 }
