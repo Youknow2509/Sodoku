485b0bb4a20835affccc0a3a68b5c6e10c2a69d9
V <lytranvinh.work@gmail.com>
2024-03-23 14:33:01 +0700

    [Upgrade]: File test LevelController

--- src/Test/TestLevel.java
+++ src/Test/TestLevel.java
@@ -1,11 +1,14 @@
 package src.Test;
 
 import src.Controller.LevelController;
+import src.Utils.Config;
 
 public class TestLevel {
     public static void main(String[] args) {
         LevelController levelController = new LevelController("admin");
         levelController.Application();
+
+        Config.closeScannerInput();
         return;
     }
 }
