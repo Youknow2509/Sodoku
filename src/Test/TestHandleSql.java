dc2de796d7b210f6d95c291163f981a62e486f29
V <lytranvinh.work@gmail.com>
2024-04-20 16:06:43 +0700

    [Add]: Add file test handle sql

--- src/Test/TestHandleSql.java
+++ src/Test/TestHandleSql.java
@@ -1,2 +1,31 @@
-package src.Test.TestView;public class TestHandleSql {
+package src.Test;
+
+import src.DataGame.Handle.HandleDataSql;
+import src.DataGame.Handle.HandleDta;
+
+import java.util.List;
+
+public class TestHandleSql {
+
+    // Show list string
+    private static void showListString(List<String> list) {
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+
+    // Main
+    public static void main(String [] arg) {
+        HandleDta handleDta = new HandleDataSql();
+
+        List<String> lUsers = handleDta.getUser();
+        List<String> lGame = handleDta.getGame();
+        List<String> lGameUser = handleDta.getGameUser();
+
+        showListString(lUsers);
+        showListString(lGame);
+        showListString(lGameUser);
+
+        return;
+    }
 }
